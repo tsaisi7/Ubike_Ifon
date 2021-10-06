f222a9058dd97141eee6ae9960990241584544d8
adam <b10709055@gapps.ntust.edu.tw>
2021-10-06 23:23:38 +0800

    Initial Commit

---
+++ Ubike_Ifon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ubike_Ifon
+//
+//  Created by Adam on 2021/10/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
